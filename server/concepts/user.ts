7a937af0cc335f599049dc642800d8590c0fb4fc
Matthew Callister <116524474+matt-cal@users.noreply.github.com>
2023-09-27 00:10:47 +0100

    Complete prep 3

--- server/concepts/user.ts
+++ server/concepts/user.ts
@@ -1,6 +1,6 @@
 import { ObjectId } from "mongodb";
 import DocCollection, { BaseDoc } from "../framework/doc";
-import { BadValuesError, NotAllowedError, NotFoundError } from "./errors";
+import { BadValuesError, NotAllowedError } from "./errors";
 
 export interface UserDoc extends BaseDoc {
   username: string;
@@ -13,7 +13,8 @@ export default class UserConcept {
   async getById(_id: ObjectId) {
     // TODO 1: Implement this method
     // Hint: check out this.users.readOne
-    throw new Error("Not implemented!");
+    const user = await this.users.readOne({ _id: _id });
+    return user;
   }
 
   async create(username: string, password: string) {
@@ -25,7 +26,8 @@ export default class UserConcept {
   async update(_id: ObjectId, update: Partial<UserDoc>) {
     // TODO 2: Implement this method
     // Hint: check out this.users.updateOne
-    throw new Error("Not implemented!");
+    const user = await this.users.updateOne({ _id: _id }, update);
+    return user;
   }
 
   // Sanitizes user object by removing password field
